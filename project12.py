60ef7d6d7a9a739ef553b98aca4d2cf03cda2075
Hasaan69 <whitefury786@gmail.com>
2025-04-06 12:34:18 +0500

    V5

---
+++ project12.py
@@ -0,0 +1,3 @@
+input_string = input('Enter a string to reverse:')
+input_string[::-1]
+print('Reversed string:', input_string[::-1])
---
+++ project13.py
@@ -0,0 +1,6 @@
+x = input('Enter household item: ')
+total_bill=2.50
+paid=4
+def return_bill(x):
+    return paid-total_bill
+print('shopkeeper have to return:',return_bill(x))
