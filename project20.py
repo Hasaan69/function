7da197c3be2fa380c2942f70f3e50bdb8f02248f
Hasaan69 <whitefury786@gmail.com>
2025-04-19 19:22:12 +0500

    v4

---
+++ project20.py
@@ -0,0 +1,6 @@
+import math
+angle = float(input("Enter trigonometic function: "))
+radian = math.radians(angle)
+print("Sine =", math.sin(radian))
+print("Cosine =", math.cos(radian))
+print("Tangent =", math.tan(radian))
