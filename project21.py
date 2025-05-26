339fe49fcc405902dae9d089f2cd2a5b7780fa3e
Hasaan69 <whitefury786@gmail.com>
2025-05-26 18:49:01 +0500

    v7

---
+++ project21.py
@@ -0,0 +1,6 @@
+from datetime import date , time , datetime
+today=date.today()
+now =datetime.now()
+print('today date is',today)
+print('current date and time is',now)
+print('data components',today.year,today.month)
