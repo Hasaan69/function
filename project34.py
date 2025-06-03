dd653399574b551fadb92702853dcd4fb0ca7f07
Hasaan69 <whitefury786@gmail.com>
2025-06-03 19:26:48 +0500

    v7

---
+++ project34.py
@@ -0,0 +1,10 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+prod1 = 1
+for c in tup1:
+    prod1 *= c
+prod2 = 1
+for c in tup2:
+    prod2 *= c
+print('The product of first tuple:', prod1)
+print('The product of second tuple:', prod2)
