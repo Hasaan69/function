619d62eff8c8ec60a9f954bb507de6a8d4f332be
Hasaan69 <whitefury786@gmail.com>
2025-06-17 17:20:10 +0500

    V3

---
+++ project37.py
@@ -0,0 +1,12 @@
+SPCMcountry_code={'USA':'+1',
+                  'Australia':'+61',
+                  'japan':'+81',
+                  'china':'+86',
+                  'india':'+91',
+                  'brazil':'+55',
+                  'france':'+33',
+                  'germany':'+49',
+                  'united kingdom':'+44',
+                  'canada':'+1',}
+print('country code for france=')
+print(SPCMcountry_code.get('japan','not found'))
