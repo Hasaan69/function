b8cf361bfd931f45266cbc8da000fd60cb6c5fad
Hasaan69 <whitefury786@gmail.com>
2025-06-18 17:08:48 +0500

    v2

---
+++ project40.py
@@ -0,0 +1,8 @@
+setx={'green','blue'}
+sety={'blue','yellow'}
+print('original set element:')
+print(setx)
+print(sety)
+print('intersection of two said sets:')
+setz=setx.intersection(sety)
+print(setz)
